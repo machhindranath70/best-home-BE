2a29835f49f46dd13ad3792ac312e8580063928b
machhindranath70 <131949567+machhindranath70@users.noreply.github.com>
2025-04-19 00:12:17 +0530

    changr host

--- best_home_location/settings.py
+++ best_home_location/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = 'django-insecure-u*os-6acimz99$slcw!$(mk35w_e%6v+7a!mjox##3jr_y9o-v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['best-home-be-1.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['best-home-be-2.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
