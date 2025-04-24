b4910fb2783e458c3e97f32d1d54239447a965f0
machhindranath70 <131949567+machhindranath70@users.noreply.github.com>
2025-04-24 11:50:29 +0530

    property servie added2

--- registration_app/views.py
+++ registration_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from rest_framework.parsers import JSONParser
 # Create your views here.
 from rest_framework.views import APIView
 from rest_framework.response import Response
